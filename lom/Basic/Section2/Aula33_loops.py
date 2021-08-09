b057ba8d59b267cabe1ece09e9c498087b0f7a5b
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-08-09 17:26:31 -0300

    Testes de loops da aula 37-38

--- lom/Basic/Section2/Aula33_loops.py
+++ lom/Basic/Section2/Aula33_loops.py
@@ -20,7 +20,30 @@ print('Fim')
 
 texto = 'O rato roeu a roupa do rei de roma'
 tamanho_texto = len(texto)
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+lista_nova = list(range(10))
 contador = 0
 while contador < tamanho_texto:
     print(f'{contador} - {texto[contador]}')
     contador += 1
+
+for letra in range(len(texto)):
+    print(letra)
+
+for letra in texto:
+    print(letra)
+
+for letra in range(len(texto)):
+    print(f'{letra} - {texto[letra]}')
+
+# del(lista[3:8])
+# print(lista)
+
+print(f'Range - {lista_nova}')
+
+# lista.insert(5, 'banana')
+
+print(*lista)
+
+print(max(lista))
+print(min(lista))
