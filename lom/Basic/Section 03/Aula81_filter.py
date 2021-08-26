b85b100ec3d4d3698bb59be4f72b4f9cddfaa621
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-08-26 20:34:38 -0300

    Aula 81 Filter

--- lom/Basic/Section 03/Aula81_filter.py
+++ lom/Basic/Section 03/Aula81_filter.py
@@ -1,6 +1,35 @@
-from dados import *
+from dados import lista, produtos, pessoas
+
+
+def filtra(produto):
+    if produto['preco'] > 50:
+        produto['status'] = 'eh caro'
+    return True
+
+
+def filtra_idade(pessoa):
+    if pessoa['idade'] > 13:
+        return True
+
 
 nova_lista = filter(lambda n: n > 5, lista)
 
 for numero in nova_lista:
     print(numero)
+
+produtos_filtrados = filter(lambda p: p['preco'] > 20, produtos)
+
+for produto in produtos_filtrados:
+    print(produto)
+
+produtos_filtrados2 = filter(filtra, produtos)
+
+print('=' * 100)
+for produto in produtos_filtrados2:
+    print(produto)
+
+pessoa_filtrada = filter(filtra_idade, pessoas)
+
+print('=' * 100)
+for pessoa in pessoa_filtrada:
+    print(pessoa)
